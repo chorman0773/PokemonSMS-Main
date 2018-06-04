82065aad3718af1d6fadf781fc5957532f2c98ec
InfernoDeity <chorman64@gmail.com>
2018-06-04 10:59:47 -0400

    Update Ability.java

--- com/google/sites/clibonlineprogram/pokemonsms/pokemon/Ability.java
+++ com/google/sites/clibonlineprogram/pokemonsms/pokemon/Ability.java
@@ -1,8 +1,8 @@
 package com.google.sites.clibonlineprogram.pokemonsms.pokemon;
 
-import java.util.ArrayList;
+import java.util.TreeSet;
 import java.util.Collections;
-import java.util.List;
+import java.util.Set;
 
 import org.luaj.vm2.LuaTable;
 
@@ -12,28 +12,51 @@ import com.google.sites.clibonlineprogram.pokemonsms.util.registry.ResourceLocat
 public class Ability {
 
 
-	private LuaTable eventHandler;
-	public final ResourceLocation loc;
-	private TextComponent name;
-	private TextComponent description;
-	private List<ResourceLocation> lifetimeShared;
+    private LuaTable eventHandler;
+    public final ResourceLocation loc;
+    private TextComponent name;
+    private TextComponent description;
+    private Set<ResourceLocation> lifetimeShared;
 
-	public Ability(LuaTable t) {
-		String loc = t.get("loc").checkjstring();
-		this.loc = new ResourceLocation(loc);
-		eventHandler = t.get("eventBus").checktable();
-		name = TextComponent.fromLua(t.get("name"));
-		description = TextComponent.fromLua(t.get("description"));
-		if(t.get("lifetimeSharedAbilities").isnil())
-			lifetimeShared = Collections.emptyList();
-		else
-		{
-			List<ResourceLocation> list = new ArrayList<>();
-			LuaTable val = t.get("lifetimeSharedAbilities").checktable();
-			for(int i = 0;i<val.length();i++)
-				list.add(new ResourceLocation(val.get(i+1).checkjstring()));
-			lifetimeShared = Collections.unmodifiableList(list);
-		}
-	}
+    public Ability(LuaTable t) {
+        String loc = t.get("loc").checkjstring();
+        this.loc = new ResourceLocation(loc);
+        eventHandler = t.get("eventBus").checktable();
+        name = TextComponent.fromLua(t.get("name"));
+        description = TextComponent.fromLua(t.get("description"));
+        if(t.get("lifetimeSharedAbilities").isnil())
+            lifetimeShared = Collections.emptyList();
+        else
+        {
+            Set<ResourceLocation> list = new TreeSet<>();
+            LuaTable val = t.get("lifetimeSharedAbilities").checktable();
+            for(int i = 0;i<val.length();i++)
+                list.add(new ResourceLocation(val.get(i+1).checkjstring()));
+            lifetimeShared = Collections.unmodifiableSet(list);
+        }
+    }
+
+    public EventBus getEventBus(){
+        return new EventBus(eventHandler);
+    }
+    
+    public boolean isLifetimeSharedWith(Ability a){
+        if(a==this)
+           return true;
+        else
+            return lifetimeShared.contains(a.loc)&&a.lifetimeShared.contains(loc);
+    }
+    
+    public ResourceLocation getLocation(){
+        return loc;   
+    }
+    
+    public TextComponent getTranslatedName(){
+        return name.getAsFormatted();
+    }
+    
+    public TextComponent getTranslatedDescription(){
+        return description.getAsFormatted();   
+    }
 
 }
